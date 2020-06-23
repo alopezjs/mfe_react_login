0ae1d9a6ce3aa024838cd1ddecca4567f4511035
AndresLopez <andres_lopez@crgl-thirdparty.com>
2020-06-22 22:08:49 -0500

    spa

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+export default class Root extends React.Component {
+  render() {
+    return (
+      <BrowserRouter basename="/react">
+        <App />
+      </BrowserRouter>
+    );
+  }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
